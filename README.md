24204c4eb9be032eec7e21283822033125273925
Mohammad Pervaiz <momoperv123@gmail.com>
2024-08-13 00:59:10 -0400

    updated README.md, fixed up html & css, added typing effect js

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Portfolio
+# Mohammad Pervaiz's Portfolio
+
+Welcome to my portfolio repository! This repository contains projects and code samples that showcase my work as a software engineer.
+
+## Projects
+
+- **People**: Developed high-impact features for a high-traffic site, including a dynamic mini journey carousel and reusable components for the 2024 Olympics.
+- **Globalizing Gender**: Built a web app for a startup supporting survivors of gender-based violence, ensuring functionality, performance, and UX.
+- **Pokedex**: Created a responsive Pokedex website with interactive features like music, sound effects, and multiple search options.
+- **Mental Health Awareness Website**: Developed a site to promote mental health awareness, featuring an engaging quiz and a petition support form.
+
+## Skills
+
+- **Languages**: HTML, CSS, JavaScript, XML
+- **Tools & Frameworks**: Git, Bootstrap, SCSS, Tailwind, Jira, Bitbucket
+
+## Contact
+
+Feel free to reach out to me via [LinkedIn](www.linkedin.com/in/mohammad-pervaiz-3781271b5).
+
+Thanks for checking out my work!
---
+++ main.js
@@ -0,0 +1,51 @@
+const phrases = [
+    "A Striving Software Developer",
+    "A Passionate Coder",
+    "An Innovative Thinker",
+    "A Lifelong Learner",
+    "A Creative Problem Solver",
+    "An Agile Developer",
+    "A Technology Enthusiast",
+    "A Team Player",
+    "An Effective Communicator",
+    "A Front-End Developer",
+    "A UI/UX Advocate",
+    "A JavaScript Ninja",
+    "A Code Perfectionist",
+    "A User-Centric Developer",
+    "A Solution-Oriented Engineer",
+  ];
+  
+  let currentPhraseIndex = 0;
+  let currentLetterIndex = 0;
+  const typingSpeed = 100;
+  const erasingSpeed = 50;
+  const delayBetweenPhrases = 2000;
+  
+  const typewriterElement = document.querySelector(".typewriter p");
+  
+  function type() {
+    if (currentLetterIndex < phrases[currentPhraseIndex].length) {
+      typewriterElement.textContent += phrases[currentPhraseIndex].charAt(currentLetterIndex);
+      currentLetterIndex++;
+      setTimeout(type, typingSpeed);
+    } else {
+      setTimeout(erase, delayBetweenPhrases);
+    }
+  }
+  
+  function erase() {
+    if (currentLetterIndex > 0) {
+      typewriterElement.textContent = phrases[currentPhraseIndex].substring(0, currentLetterIndex - 1);
+      currentLetterIndex--;
+      setTimeout(erase, erasingSpeed);
+    } else {
+      currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+      setTimeout(type, typingSpeed);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(type, delayBetweenPhrases);
+  });
+  
